c58ecf2aa4b7e35189ad9021502f47eebece4577
Marcin Borek <marcin@preglife.com>
2021-11-24 15:10:25 +0100

    Initial commit

---
+++ TrackingSampleApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TrackingSampleApp
+//
+//  Created by Marcin Borek on 23/11/2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
