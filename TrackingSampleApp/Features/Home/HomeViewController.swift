3896fc0cbb7a19d4004331f093796a220a340b69
Marcin Borek <marcin@preglife.com>
2021-11-26 23:25:06 +0100

    Add sample table view cell to "Home"

--- TrackingSampleApp/Features/Home/HomeViewController.swift
+++ TrackingSampleApp/Features/Home/HomeViewController.swift
@@ -7,10 +7,67 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController {
+
+struct Widget {
+    let title: String
+    let shortDescription: String
+}
+
+final class HomeViewController: UITableViewController {
+    private let widgets: [Widget] = {
+        var widgets = [Widget]()
+        widgets.append(Widget(title: "Title 1", shortDescription: "Short description 1"))
+        widgets.append(Widget(title: "Title 2", shortDescription: "Short description 2"))
+        return widgets
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
         view.backgroundColor = .green
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(
+            TableViewCell.self,
+            forCellReuseIdentifier: ReuseIdentifier.tableCell
+        )
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return widgets.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: ReuseIdentifier.tableCell,
+                for: indexPath as IndexPath
+            ) as? TableViewCell
+            if cell != nil {
+                return cell!
+            }
+            return UITableViewCell(
+                style: UITableViewCell.CellStyle.subtitle,
+                reuseIdentifier: ReuseIdentifier.tableCell
+            )
+        }()
+        
+        cell.imageView?.image = UIImage(named: "article.png")
+        cell.textLabel?.text = "Text"
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.detailTextLabel?.text = "Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text"
+        return cell
+    }
+}
+
+extension HomeViewController {
+    private enum ReuseIdentifier {
+        static let tableCell = "tableCell"
     }
 }
---
+++ TrackingSampleApp/Features/Home/Views/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  TrackingSampleApp
+//
+//  Created by Marcin Borek on 25/11/2021.
+//
+
+import UIKit
+
+final class TableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
